9ca2189cf5dcb0e1ecf125f42f7ea658a47a387e
Luksys3 <thelukys3@gmail.com>
2017-10-29 13:46:16 +0200

    update

--- sketch.js
+++ sketch.js
@@ -1,8 +1,26 @@
 
 function setup() {
-    
+    createCanvas(850, 700);
 }
 
 function draw() {
     background(51);
+
+    // Map
+    strokeWeight(0);
+    fill(0, 150, 0);
+    rect(0, 50, 650, 700);
+
+    // Top bar
+    fill(255);
+    textSize(20);
+    text("Score:", 20, 32);
+    text("Wave:", 220, 32);
+    text("Money:", 420, 32);
+
+    // Top bar
+    fill(255);
+    textSize(20);
+    text("Ground", 680, 100);
+    text("Air", 680, 250);
 }
